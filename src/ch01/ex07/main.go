1ccdde08b634b0e7eaafb75a30949ab969be4db4
Yoshiki Shibata <yshibata@ca2.so-net.ne.jp>
2015-06-14 16:47:19 +0900

    added

---
+++ src/ch01/ex07/main.go
@@ -0,0 +1,32 @@
+//!+
+// Curl displays the content found at a URL.
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	url := os.Args[1]
+	if !strings.HasPrefix(url, "http://") {
+		url = "http://" + url
+	}
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "curl: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println("Response Code =", resp.StatusCode)
+	_, err = io.Copy(os.Stdout, resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "curl: copying %s: %v\n", url, err)
+		os.Exit(1)
+	}
+}
+
+//!-
