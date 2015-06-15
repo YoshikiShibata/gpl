290518baa71b2d2c7672b4a620d6127cf3450ab3
Yoshiki Shibata <yshibata@ca2.so-net.ne.jp>
2015-06-16 04:59:56 +0900

    added

---
+++ src/ch01/ex10/lissajous.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
+	"net/http"
+)
+
+var palette = []color.Color{color.Black,
+	color.RGBA{0xff, 0x00, 0x00, 0xff},
+	color.RGBA{0x00, 0xff, 0x00, 0xff},
+	color.RGBA{0x00, 0x00, 0xff, 0xff}}
+
+const (
+	whiteIndex = 0 // first color in palette
+)
+
+func lissajous(out io.Writer, cycles float64) {
+	const (
+		// cycles  = 5     // number of complete x oscillator revolutions
+		res     = 0.001 // angular resolution
+		size    = 100   // canvas covers -size..size
+		nframes = 64    // number of animcation frames
+		delay   = 8     // delay between frames in 10ms units
+		fill    = 0.9   // fraction of image canvas to fill
+	)
+
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // phase difference
+	noOfColors := len(palette) - 1
+	for i := 0; i < nframes; i++ {
+		colorIndex := uint8(i%noOfColors + 1)
+		img := image.NewPaletted(image.Rect(0, 0, 2*size, 2*size), palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(fill*x*size),
+				size+int(fill*y*size),
+				colorIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim)
+}
+
+func main() {
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		lissajous(w, 5)
+	}
+	http.HandleFunc("/", handler)
+	http.ListenAndServe("localhost:8000", nil)
+	// NB: no error handling
+}
