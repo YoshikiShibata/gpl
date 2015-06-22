f6d0e1f19b6888e42379c8561246caa445d891d3
Yoshiki Shibata <yshibata@ca2.so-net.ne.jp>
2015-06-23 06:49:08 +0900

    updated

--- src/ch03/ex02/surface.go
+++ src/ch03/ex02/surface.go
@@ -38,4 +38,15 @@ func main() {
 	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
 		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
 		"width='%d' height='%d'>", width, height)
+	for i := 0; i < cells; i++ {
+		for j := 0; j < cells; j++ {
+			ax, ay := corner(i+1, j)
+			bx, by := corner(i, j)
+			cx, cy := corner(i, j+1)
+			dx, dy := corner(i+1, j+2)
+			fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
+				ax, ay, bx, by, cx, cy, dx, dy)
+		}
+	}
+	fmt.Println("</svg>")
 }
