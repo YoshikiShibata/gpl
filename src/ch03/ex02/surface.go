84162581de8cad2148d38d85e5c1362ea436faef
Yoshiki Shibata <yoshiki.shibata@nts.ricoh.co.jp>
2015-06-23 13:06:27 +0900

    ex02 done

--- src/ch03/ex02/surface.go
+++ src/ch03/ex02/surface.go
@@ -12,6 +12,12 @@ const (
 	xyscale       = width / 2 / xyrange // pixels per x or y unit
 	zscale        = height * 0.4        // pixels per z unit
 	angle         = math.Pi / 6         // angle of x, y axes (=30°)
+
+	maxHeight = 1.0
+	minHeight = -0.22
+
+	red  = 0x00ff0000
+	blue = 0x000000ff
 )
 
 var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
@@ -34,18 +40,38 @@ func corner(i, j int) (float64, float64, float64) {
 	return sx, sy, z
 }
 
+func color(h1, h2, h3, h4 float64) string {
+	height := (h1 + h2 + h3 + h4) / 4
+	if height > maxHeight || minHeight > height {
+		panic(fmt.Sprintf("illegal height : %g", height))
+	}
+
+	delta := uint32((maxHeight - height) / (maxHeight - minHeight) * 255)
+
+	c := (red - delta<<16) + delta
+	return prependZeros(fmt.Sprintf("%X", c))
+}
+
+func prependZeros(hex string) string {
+	result := hex
+	for i := len(hex); i < 6; i++ {
+		result = "0" + result
+	}
+	return result
+}
+
 func main() {
 	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
 		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
 		"width='%d' height='%d'>", width, height)
 	for i := 0; i < cells; i++ {
 		for j := 0; j < cells; j++ {
-			ax, ay, _ := corner(i+1, j)
-			bx, by, _ := corner(i, j)
-			cx, cy, _ := corner(i, j+1)
-			dx, dy, _ := corner(i+1, j+2)
-			fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g' fill='#ff0000' />\n",
-				ax, ay, bx, by, cx, cy, dx, dy)
+			ax, ay, h1 := corner(i+1, j)
+			bx, by, h2 := corner(i, j)
+			cx, cy, h3 := corner(i, j+1)
+			dx, dy, h4 := corner(i+1, j+2)
+			fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g' fill='#%s' />\n",
+				ax, ay, bx, by, cx, cy, dx, dy, color(h1, h2, h3, h4))
 		}
 	}
 	fmt.Println("</svg>")
