6ae4f78c2c39e4a396b015dbe0cd777849541159
Yoshiki Shibata <yshibata@ca2.so-net.ne.jp>
2015-06-25 05:59:42 +0900

    backup

--- src/ch03/ex04/mandelbrot.go
+++ src/ch03/ex04/mandelbrot.go
@@ -14,7 +14,7 @@ func mandelbrot(z complex128) color.Color {
 	var v complex128
 
 	for n := uint8(0); n < iterations; n++ {
-		v = v + v + z
+		v = v*v + z
 		if cmplx.Abs(v) > 2 {
 			return color.Gray{255 - contrast*n}
 		}
