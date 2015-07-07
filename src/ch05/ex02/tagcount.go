a833dd8b71f0e191be9d509a55a9a2bc351a01a3
Yoshiki Shibata <yshibata@ca2.so-net.ne.jp>
2015-07-07 11:25:09 +0900

    ex02 done

---
+++ src/ch05/ex02/tagcount.go
@@ -0,0 +1,35 @@
+// Copyright (C) 2015 Yoshiki Shibata. All rights reserved.
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+func visitTag(n *html.Node, tagCount map[string]int) {
+	if n.Type == html.ElementNode {
+		tagCount[n.Data]++
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		visitTag(c, tagCount)
+	}
+}
+
+func main() {
+	counts := make(map[string]int)
+
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "tagcount: %v\n", err)
+		os.Exit(1)
+	}
+
+	visitTag(doc, counts)
+
+	for t, c := range counts {
+		fmt.Printf("%10s: %d\n", t, c)
+	}
+}
