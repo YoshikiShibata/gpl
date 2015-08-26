139bd77e93967c3b8db71f4bb7f769182dac544b
Yoshiki Shibata <yshibata@ca2.so-net.ne.jp>
2015-08-27 08:49:33 +0900

    added

---
+++ src/ch01/ex09/fetch.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	url := os.Args[1]
+	if !strings.HasPrefix(url, "http://") {
+		url = "http://" + url
+	}
+	resp, err := http.Get(url)
+
+	fmt.Println("Status Code =", resp.StatusCode)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+		os.Exit(1)
+	}
+
+	_, err = io.Copy(os.Stdout, resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "fetch: copying %s: %v\n", url, err)
+		os.Exit(1)
+	}
+}
