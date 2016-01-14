78b6e3c4e8b8bf358dc5ab5570e77374bc079abc
Yoshiki Shibata <yshibata@ca2.so-net.ne.jp>
2016-01-14 16:26:37 +0900

    Not Implemented Yet

---
+++ src/ch04/ex11/issue.go
@@ -0,0 +1,54 @@
+// Copyright © 2016 Yoshiki Shibata
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"syscall"
+
+	"golang.org/x/crypto/ssh/terminal"
+)
+
+// You need to get terminal package by the following command:
+//  go get golang.org/x/crypto/ssh
+
+// Usage
+// 1. Create an issue
+// 		issue REPOSITORY [-create -title TITLE -body BODY]
+//		if -body is omitted, then an editor will be invoked.
+//
+// 2. Delete an issue
+//		issue REPOSITORY [-delete -issue ISSUE_NO]
+//
+// 3. Print an issue
+//		issue REPOSITORY [-print -issue ISSUE_NO]
+//
+// 4. Edit an issue
+// 		issue REPOSITORY [-edit -issue ISSUE_NO -title TITLE -body BODY -state [open | close]]
+
+func main() {
+	user, password := credentials()
+
+	fmt.Printf("use = %s, password = %s\n", user, password)
+
+	panic("Not Implemented Yet")
+}
+
+func credentials() (string, string) {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Username: ")
+	username, _ := reader.ReadString('\n')
+
+	fmt.Print("Password: ")
+	bytePassword, err := terminal.ReadPassword(int(syscall.Stdin))
+	if err != nil {
+		fmt.Println("\nPassword Error: %v\n", err)
+	}
+	password := string(bytePassword)
+
+	return strings.TrimSpace(username), strings.TrimSpace(password)
+}
