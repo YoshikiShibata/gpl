62016cfbec6c07a269af980e2690e0b9f51cb77d
Yoshiki Shibata <yshibata@ca2.so-net.ne.jp>
2016-02-10 15:33:18 +0900

    added all.bash

---
+++ src/ch04/ex13/all.bash
@@ -0,0 +1,21 @@
+go build -o poster
+
+rm *.jpg
+
+./poster "Back To The Future"
+./poster "Back To The Future II"
+./poster "Back To The Future III"
+
+./poster "Star Trek I"
+./poster "Star Trek II"
+./poster "Star Trek III"
+./poster "Star Trek IV"
+./poster "Star Trek V"
+./poster "Star Trek VI"
+./poster "Star Trek VII"
+./poster "Star Trek VIII"
+./poster "Star Trek IX"
+./poster "Star Trek X"
+
+./poster "Dave"
+./poster "Sleepless in Seattle"
