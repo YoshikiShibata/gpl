bccb459f1bb25d3a271fb8dc9b0723633d85b81b
Yoshiki Shibata <yshibata@ca2.so-net.ne.jp>
2016-03-01 12:57:49 +0900

    Not Done Yet

---
+++ src/ch09/ex03/memo/memo.go
@@ -0,0 +1,107 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// Copyright © 2016 Yoshiki Shibata. All rights reserved.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// Package memo provides a concurrency-safe non-blocking memoization
+// of a function.  Requests for different keys proceed in parallel.
+// Concurrent requests for the same key block until the first completes.
+// This implementation uses a monitor goroutine.
+package memo
+
+import "errors"
+
+// Func is the type of the function to memoize.
+// Closing done channel means the cancellation of this func.
+type Func func(key string, done <-chan struct{}) (interface{}, error)
+
+// A result is the result of calling a Func.
+type result struct {
+	value interface{}
+	err   error
+}
+
+// ErrCancelled indicates that Get operation is cancelled by the client
+// and no cache is updated.
+var ErrCancelled = errors.New("cancelled")
+
+type entry struct {
+	res   result
+	ready chan struct{} // closed when res is ready
+}
+
+// A request is a message requesting that the Func be applied to key.
+type request struct {
+	key      string
+	response chan<- result   // the client wants a single result
+	done     <-chan struct{} // the cleint wants to cancel when closed
+}
+
+type Memo struct{ requests chan request }
+
+// New returns a memoization of f.  Clients must subsequently call Close.
+func New(f Func) *Memo {
+	memo := &Memo{requests: make(chan request)}
+	go memo.server(f)
+	return memo
+}
+
+func (memo *Memo) Get(key string, done <-chan struct{}) (interface{}, error) {
+	response := make(chan result)
+	memo.requests <- request{key, response, done}
+	res := <-response
+	if res.err == ErrCancelled {
+		select {
+		case <-done:
+			// client cancelled
+			return res.value, res.err
+		default:
+			// client did not cancelled. Retry again.
+			return memo.Get(key, done)
+		}
+	}
+	return res.value, res.err
+}
+
+func (memo *Memo) Close() { close(memo.requests) }
+
+func (memo *Memo) server(f Func) {
+	cache := make(map[string]*entry)
+	for req := range memo.requests {
+		e := cache[req.key]
+
+		if e != nil {
+			select {
+			case <-e.ready:
+				if e.res.err == ErrCancelled {
+					// Previous one was cancelled
+					delete(cache, req.key)
+					e = nil
+				}
+			default:
+				// do nothing
+			}
+		}
+
+		if e == nil {
+			// This is the first request for this key.
+			e = &entry{ready: make(chan struct{})}
+			cache[req.key] = e
+			go e.call(f, req.key, req.done) // call f(key)
+		}
+		go e.deliver(req.response)
+	}
+}
+
+func (e *entry) call(f Func, key string, done <-chan struct{}) {
+	// Evaluate the function.
+	e.res.value, e.res.err = f(key, done)
+	// Broadcast the ready condition.
+	close(e.ready)
+}
+
+func (e *entry) deliver(response chan<- result) {
+	// Wait for the ready condition.
+	<-e.ready
+	// Send the result to the client.
+	response <- e.res
+}
---
+++ src/ch09/ex03/memo/memo_test.go
@@ -0,0 +1,25 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+package memo_test
+
+import (
+	"testing"
+
+	"ch09/ex03/memo"
+	"ch09/ex03/memotest"
+)
+
+var httpGetBody = memotest.HTTPGetBody
+
+func Test(t *testing.T) {
+	m := memo.New(httpGetBody)
+	defer m.Close()
+	memotest.Sequential(t, m)
+}
+
+func TestConcurrent(t *testing.T) {
+	m := memo.New(httpGetBody)
+	defer m.Close()
+	memotest.Concurrent(t, m)
+}
