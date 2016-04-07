49c63add0f22510558e67915037825a7741670c1
Yoshiki Shibata <yshibata@ca2.so-net.ne.jp>
2016-04-07 09:59:55 +0900

    ex05 done

--- src/ch07/ex05/limitReader.go
+++ src/ch07/ex05/limitReader.go
@@ -4,6 +4,33 @@ package main
 
 import "io"
 
+type limitedReader struct {
+	r     io.Reader
+	limit int64
+	next  int
+}
+
+func (lr *limitedReader) Read(p []byte) (int, error) {
+	if len(p) == 0 {
+		return 0, nil
+	}
+
+	if int64(lr.next) >= lr.limit {
+		return 0, io.EOF
+	}
+
+	nbytes := int(lr.limit - int64(lr.next))
+	if nbytes > len(p) {
+		nbytes = len(p)
+	}
+	n, err := lr.r.Read(p[:nbytes])
+	if err != nil {
+		return n, err
+	}
+	lr.next += nbytes
+	return n, nil
+}
+
 func LimitReader(r io.Reader, n int64) io.Reader {
-	panic("Not Implemented Yet")
+	return &limitedReader{r, n, 0}
 }
--- src/ch07/ex05/limitReader_test.go
+++ src/ch07/ex05/limitReader_test.go
@@ -2,16 +2,45 @@
 
 package main
 
-import "testing"
+import (
+	"io"
+	"strings"
+	"testing"
+)
 
 func TestLimitReader(t *testing.T) {
-	for _, test := range []struct {
+topLoop:
+	for _, tc := range []struct {
 		data    string
 		bufSize int
 		limit   int64
 	}{
+		{"0123456789", 0, 3},
+		{"0123456789", 1, 0},
 		{"0123456789", 10, 3},
+		{"", 10, 3},
 	} {
+		r1 := strings.NewReader(tc.data)
+		r2 := strings.NewReader(tc.data)
+		b1 := make([]byte, tc.bufSize)
+		b2 := make([]byte, tc.bufSize)
 
+		lr1 := LimitReader(r1, tc.limit)
+		lr2 := io.LimitReader(r2, tc.limit)
+
+		for i := 0; i < 2; i++ {
+			n1, err1 := lr1.Read(b1)
+			n2, err2 := lr2.Read(b2)
+			t.Logf("n1 = %d, err1 = %v\n", n1, err1)
+			t.Logf("n2 = %d, err2 = %v\n", n2, err2)
+			if err1 != err2 {
+				t.Errorf("err1=%v, err2=%v", err1, err2)
+				continue topLoop
+			}
+			if n1 != n2 {
+				t.Errorf("n1=%d, n2=%d", n1, n2)
+				continue topLoop
+			}
+		}
 	}
 }
