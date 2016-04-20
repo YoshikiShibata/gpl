4f27acd80f6643f5ec6d0f7796de3f302600e74d
Yoshiki Shibata <yshibata@ca2.so-net.ne.jp>
2016-04-20 09:59:47 +0900

    fixed the incorrect function name

--- src/ch05/ex19/main.go
+++ src/ch05/ex19/main.go
@@ -5,10 +5,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("sqrt(2) = %d\n", sqrt(2))
+	fmt.Printf("square(2) = %d\n", square(2))
 }
 
-func sqrt(n int) (r int) {
+func square(n int) (r int) {
 	defer func() {
 		if p := recover(); p != nil {
 			r = p.(int)
