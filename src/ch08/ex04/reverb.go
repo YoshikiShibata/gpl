28bfc4e8f7532e930c771c271d8ccc5be714dbf2
Yoshiki Shibata <yshibata@ca2.so-net.ne.jp>
2016-06-14 10:43:05 +0900

    ex04 done

---
+++ src/ch08/ex04/reverb.go
@@ -0,0 +1,56 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// Copyright © 2016 Yoshiki Shibata. All rights reserved.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// Reverb2 is a TCP server that simulates an echo.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"sync"
+	"time"
+)
+
+func echo(c net.Conn, shout string, delay time.Duration) {
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", shout)
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+}
+
+func handleConn(c net.Conn) {
+	input := bufio.NewScanner(c)
+	var wg sync.WaitGroup
+
+	for input.Scan() {
+		wg.Add(1)
+		go func() {
+			echo(c, input.Text(), 1*time.Second)
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+
+	// NOTE: ignoring potential errors from input.Err()
+	c.Close()
+}
+
+func main() {
+	l, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Print(err) // e.g., connection aborted
+			continue
+		}
+		go handleConn(conn)
+	}
+}
