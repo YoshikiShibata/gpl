0979327b110b94e2253eabc749d43b3fad9c733f
Yoshiki Shibata <yshibata@ca2.so-net.ne.jp>
2016-07-06 12:05:50 +0900

    Not Done Yet

--- src/ch08/ex07/mirror.go
+++ src/ch08/ex07/mirror.go
@@ -11,48 +11,61 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
 
 	"golang.org/x/net/html"
 )
 
+const (
+	ContentType = "Content-Type"
+	TextHTML    = "text/html"
+)
+
 func main() {
-	Extract(os.Args[1])
+	if err := Extract(os.Args[1]); err != nil {
+		fmt.Printf("%v\n", err)
+	}
 }
 
 // Extract makes an HTTP GET request to the specified URL, parses
 // the response as HTML, and returns the links in the HTML document.
-func Extract(url string) ([]string, error) {
+func Extract(url string) error {
 	resp, err := http.Get(url)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	host := resp.Request.URL.Host
-	fmt.Printf("host = %s\n", host)
+	path := resp.Request.URL.Path
+	scheme := resp.Request.URL.Scheme
+	hostURL := scheme + "://" + host
+	fmt.Printf("scheme = %s, host = %s, path = %s\n", scheme, host, path)
+	fmt.Printf("hostURL = %s\n", hostURL)
+	fmt.Printf("%#v\n", resp.Header)
+
 	if err := os.Mkdir(host, os.ModePerm); err != nil {
 		if os.IsExist(err) {
 			fmt.Printf("%s directory exists! Please delete it\n", host)
-			return nil, nil
+			return nil
 		}
 
 		fmt.Printf("os.Mkdir : %v\n", err)
-		return nil, err
+		return err
 	}
 
 	if resp.StatusCode != http.StatusOK {
 		resp.Body.Close()
-		return nil, fmt.Errorf("getting %s: %s", url, resp.Status)
+		return fmt.Errorf("getting %s: %s", url, resp.Status)
 	}
 
-	fmt.Printf("%#v\n", resp.Header)
+	contentType := extractContentType(resp.Header)
 
 	doc, err := html.Parse(resp.Body)
 	resp.Body.Close()
 	if err != nil {
-		return nil, fmt.Errorf("parsing %s as HTML: %v", url, err)
+		return fmt.Errorf("parsing %s as HTML: %v", url, err)
 	}
 
-	var links []string
 	visitNode := func(n *html.Node) {
 		if n.Type == html.ElementNode && n.Data == "a" {
 			for i, a := range n.Attr {
@@ -63,24 +76,34 @@ func Extract(url string) ([]string, error) {
 				if err != nil {
 					continue // ignore bad URLs
 				}
-				if a.Val != link.String() {
-					fmt.Fprintf(os.Stderr, "[%s] vs [%s]\n", a.Val, link.String())
-					n.Attr[i].Val = link.String()
+				if strings.HasPrefix(link.String(), hostURL) {
+					// replace href because this is the same host
+					n.Attr[i].Val = "file:" + a.Val
+					if a.Val != "/" && link.String() != hostURL {
+						extractAsFile(link.String(), host+"/"+a.Val)
+					}
 				}
-				links = append(links, link.String())
 			}
 		}
 	}
 	forEachNode(doc, visitNode, nil)
 
-	f, err := os.Create(host + "/root.html")
-	if err != nil {
-		fmt.Printf("%v\n", err)
-		return nil, err
+	if path == "/" && contentType[0] == TextHTML {
+		f, err := os.Create(host + "/root.html")
+		if err != nil {
+			fmt.Printf("%v\n", err)
+			return err
+		}
+		defer f.Close()
+		html.Render(f, doc)
 	}
-	defer f.Close()
-	html.Render(f, doc)
-	return links, nil
+
+	fmt.Printf("path = %s, contentType[0] = %s\n", path, contentType[0])
+	return nil
+}
+
+func extractAsFile(url, path string) {
+	fmt.Printf("Contents of %s should be stored as %s\n", url, path)
 }
 
 // Copied from gopl.io/ch5/outline2.
@@ -94,4 +117,12 @@ func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
 	if post != nil {
 		post(n)
 	}
+}
+
+func extractContentType(header http.Header) []string {
+	contentType, ok := header[ContentType]
+	if !ok {
+		return nil
+	}
+	return strings.Split(contentType[0], ";")
 }
