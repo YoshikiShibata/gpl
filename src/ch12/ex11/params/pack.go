36330f2b11cf63957b343a58e6a317aa11ad9440
Yoshiki Shibata <yshibata@ca2.so-net.ne.jp>
2016-09-04 16:25:09 +0900

    ex11 done

--- src/ch12/ex11/params/pack.go
+++ src/ch12/ex11/params/pack.go
@@ -1,5 +1,53 @@
+// Copyright © 2016 Yoshiki Shibata. All rights reserved.
+
 package params
 
-func Pack(ptr interface{}) (string, error) {
-	panic("not implemented ye")
+import (
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+//+ Exercise 12.11
+
+func Pack(ptr interface{}) string {
+	var result []string
+
+	v := reflect.ValueOf(ptr).Elem() // the struct variable
+	for i := 0; i < v.NumField(); i++ {
+		fieldInfo := v.Type().Field(i) // a reflect.StructField
+		tag := fieldInfo.Tag           // a reflect.StructTag
+		name := tag.Get("http")
+		if name == "" {
+			name = strings.ToLower(fieldInfo.Name)
+		}
+		result = append(result, toParam(name, v.Field(i)))
+	}
+	return strings.Join(result, "&")
 }
+
+func toParam(name string, v reflect.Value) string {
+	switch v.Kind() {
+	case reflect.String:
+		return fmt.Sprintf("%s=%s", name, v.String())
+	case reflect.Int:
+		return fmt.Sprintf("%s=%d", name, v.Int())
+	case reflect.Bool:
+		if v.Bool() {
+			return fmt.Sprintf("%s=true", name)
+		} else {
+			return fmt.Sprintf("%s=false", name)
+		}
+	case reflect.Array, reflect.Slice:
+		var result []string
+		for i := 0; i < v.Len(); i++ {
+			result = append(result, toParam(name, v.Index(i)))
+		}
+		return strings.Join(result, "&")
+
+	default:
+		panic(fmt.Sprintf("Unspported Kind = %d", v.Kind()))
+	}
+}
+
+//- Exercise 12.11
--- src/ch12/ex11/params/pack_test.go
+++ src/ch12/ex11/params/pack_test.go
@@ -1,6 +1,10 @@
+// Copyright © 2016 Yoshiki Shibata. All rights reserved.
+
 package params
 
 import "testing"
+
+//+ Exercise 12.11
 
 type data struct {
 	Labels     []string `http:"l"`
@@ -8,21 +12,44 @@ type data struct {
 	Exact      bool     `http:"x"`
 }
 
+type notagdata struct {
+	Labels     []string
+	MaxResults int
+	Exact      bool
+}
+
 func TestPack(t *testing.T) {
 	for _, test := range []struct {
 		d      data
 		params string
-	}{{data{Labels: []string{"golang"}, MaxResults: 10, Exact: true},
-		"l=golang&max=10&x=true"},
+	}{
+		{data{Labels: []string{"golang"}, MaxResults: 10, Exact: true},
+			"l=golang&max=10&x=true"},
+		{data{Labels: []string{"golang", "programming"}, MaxResults: 10, Exact: true},
+			"l=golang&l=programming&max=10&x=true"},
 	} {
-		params, err := Pack(&test.d)
-		if err != nil {
-			t.Errorf("%v\n", err)
-			continue
-		}
+		params := Pack(&test.d)
 
 		if params != test.params {
 			t.Error("params = %q, but want %q\n", params, test.params)
 		}
 	}
 }
+
+func TestPackNoTag(t *testing.T) {
+	for _, test := range []struct {
+		d      notagdata
+		params string
+	}{
+		{notagdata{Labels: []string{"golang", "programming"}, MaxResults: 10, Exact: true},
+			"labels=golang&labels=programming&maxresults=10&exact=true"},
+	} {
+		params := Pack(&test.d)
+
+		if params != test.params {
+			t.Error("params = %q, but want %q\n", params, test.params)
+		}
+	}
+}
+
+//- Exercise 12.11
