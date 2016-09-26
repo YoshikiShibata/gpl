20bf0c64131c77074de437d0536036a159f279a7
Yoshiki Shibata <yshibata@ca2.so-net.ne.jp>
2016-09-26 14:55:54 +0900

    updated

--- src/ch03/ex07/newton.go
+++ src/ch03/ex07/newton.go
@@ -41,9 +41,15 @@ func newton(z complex128) color.Color {
 	const contrast = 7
 	for i := uint8(0); i < iterations; i++ {
 		z -= (z - 1/(z*z*z)) / 4
-		if cmplx.Abs(z*z*z*z-1) < 1e-6 {
-			// return color.Gray{255 - contrast*i}
-			return color.RGBA{255 - contrast*i, contrast * i, 0, 0xff}
+		switch {
+		case cmplx.Abs((1+0i)-z) < 1e-6:
+			return color.RGBA{255 - contrast*i, 0, 0, 0xff}
+		case cmplx.Abs((-1+0i)-z) < 1e-6:
+			return color.RGBA{0, 255 - contrast*i, 0, 0xff}
+		case cmplx.Abs((0+1i)-z) < 1e-6:
+			return color.RGBA{0, 0, 255 - contrast*i, 0xff}
+		case cmplx.Abs((0-1i)-z) < 1e-6:
+			return color.RGBA{255 - contrast*i, 255 - contrast*i, 0, 0xff}
 		}
 	}
 	return color.Black
