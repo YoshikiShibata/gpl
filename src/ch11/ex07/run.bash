11c759c12310772c05af4d5d2287c8adc5001a99
Yoshiki Shibata <yshibata@ca2.so-net.ne.jp>
2017-01-27 16:15:16 +0900

    Updated to run both 32 bits and 64 bits

--- src/ch11/ex07/run.bash
+++ src/ch11/ex07/run.bash
@@ -4,7 +4,25 @@
 
 go test -bench=.
 
+echo "32 Bit version"
+GOARCH=386 go test -bench=. bench_test.go intset.go mapintset.go
+
 # BenchmarkAdd_IntSet-8                  50000         51907 ns/op
 # BenchmarkAdd_MapIntSet-8               10000        183672 ns/op
 # BenchmarkUnitonWith_IntSet-8           20000         82875 ns/op
 # BenchmarkUnionWith_MapIntSet-8          3000        437941 ns/op
+
+# 2017.01.27 Go 1.8 (almost)
+# BenchmarkAdd_IntSet-4            	   50000	     37298 ns/op
+# BenchmarkAdd_MapIntSet-4         	   10000	    133083 ns/op
+# BenchmarkUnitonWith_IntSet-4     	   20000	     63415 ns/op
+# BenchmarkUnionWith_MapIntSet-4   	    5000	    372974 ns/op
+# PASS
+# ok  	ch11/ex07	7.423s
+# 32 Bit version
+# BenchmarkAdd_IntSet-4            	   30000	     40636 ns/op
+# BenchmarkAdd_MapIntSet-4         	   10000	    179372 ns/op
+# BenchmarkUnitonWith_IntSet-4     	   20000	     68477 ns/op
+# BenchmarkUnionWith_MapIntSet-4   	    3000	    462411 ns/op
+# PASS
+# ok  	command-line-arguments	6.977s
