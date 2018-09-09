9131e85399a1f2a3ee99313142c3cd5d4f5649f2
Yoshiki Shibata <yshibata@ca2.so-net.ne.jp>
2018-09-10 06:42:27 +0900

    Refactored

--- ch05/ex02/tagcount.go
+++ ch05/ex02/tagcount.go
@@ -1,4 +1,4 @@
-// Copyright © 2016 Yoshiki Shibata. All rights reserved.
+// Copyright © 2016, 2018 Yoshiki Shibata. All rights reserved.
 
 package main
 
@@ -19,14 +19,13 @@ func visitTag(n *html.Node, tagCount map[string]int) {
 }
 
 func main() {
-	counts := make(map[string]int)
-
 	doc, err := html.Parse(os.Stdin)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "tagcount: %v\n", err)
 		os.Exit(1)
 	}
 
+	counts := make(map[string]int)
 	visitTag(doc, counts)
 
 	for t, c := range counts {
