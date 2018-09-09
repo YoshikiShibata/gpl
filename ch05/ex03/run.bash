4fe4b6f1e868595163b45941a8fdea143b1d6778
Yoshiki Shibata <yshibata@ca2.so-net.ne.jp>
2018-09-10 06:51:20 +0900

    updated

--- ch05/ex03/run.bash
+++ ch05/ex03/run.bash
@@ -1,7 +1,10 @@
-#!/bin/bash
+#!/bin/bash -x
 
-# Copyright (C) 2016 Yoshiki Shibata. All rights reserved.
+# Copyright (C) 2016, 2018 Yoshiki Shibata. All rights reserved.
 
 go build gopl.io/ch1/fetch
 go build text.go
 ./fetch https://golang.org | ./text
+
+# clean
+rm fetch text
--- ch05/ex04/run.bash
+++ ch05/ex04/run.bash
@@ -1,7 +1,10 @@
-#!/bin/bash
+#!/bin/bash -x
 
-# Copyright (C) 2016 Yoshiki Shibata. All rights reserved.
+# Copyright (C) 2016, 2018 Yoshiki Shibata. All rights reserved.
 
 go build gopl.io/ch1/fetch
 go build outline.go
 ./fetch https://golang.org | ./outline
+
+# clean
+rm fetch outline
