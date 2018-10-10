c9755e719468e9f98ba372578c4379fe0d4b6d68
Yoshiki Shibata <yoshiki.shibata@gmail.com>
2018-10-10 10:55:50 +0900

    Added UTC

--- ch08/ex01/run.bash
+++ ch08/ex01/run.bash
@@ -16,6 +16,7 @@ killall clock2
 TZ=US/Eastern    ./clock2 -port 8010 &
 TZ=Asia/Tokyo    ./clock2 -port 8020 &
 TZ=Europe/London ./clock2 -port 8030 &
+TZ=UTC ./clock2 -port 8040&
 
 echo ""
-./clockwall NewYork=localhost:8010 Tokyo=localhost:8020 London=localhost:8030
+./clockwall NewYork=localhost:8010 Tokyo=localhost:8020 London=localhost:8030 UTC=localhost:8040
