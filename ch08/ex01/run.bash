06c03f5e9d0ff6395f47299d51aa388b67cff54c
Yoshiki Shibata <yoshiki.shibata@gmail.com>
2019-05-20 14:04:06 +0900

    Added sleep 3

--- ch08/ex01/run.bash
+++ ch08/ex01/run.bash
@@ -19,5 +19,6 @@ TZ=Asia/Tokyo    ./clock2 -port 8030 &
 TZ=Europe/London ./clock2 -port 8040 &
 TZ=UTC ./clock2 -port 8050&
 
+sleep 3
 echo ""
 ./clockwall "Palo Alto"=localhost:8010 NewYork=localhost:8020 Tokyo=localhost:8030 London=localhost:8040 UTC=localhost:8050
