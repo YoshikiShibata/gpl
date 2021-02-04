e32f61b78f0398a83a1830b09ab6dc3d0cd2d4eb
Yoshiki Shibata <yoshikishibata@YoshikinoMacBook-Pro.local>
2021-02-05 07:11:43 +0900

    commented out lines for 32bits

--- ch11/ex07/run.bash
+++ ch11/ex07/run.bash
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# Copyright (C) 2016 Yoshiki Shibata. All rights reserved.
+# Copyright (C) 2016, 2021 Yoshiki Shibata. All rights reserved.
 
 go test -bench=.
 
-echo "32 Bit version"
-GOARCH=386 go test -bench=. bench_test.go intset.go mapintset.go
+# Now macOS doesn't support 32 bits
+# echo "32 Bit version"
+# GOARCH=386 go test -bench=. bench_test.go intset.go mapintset.go
 
 # BenchmarkAdd_IntSet-8                  50000         51907 ns/op
 # BenchmarkAdd_MapIntSet-8               10000        183672 ns/op
